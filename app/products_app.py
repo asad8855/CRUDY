f5dabd72933c8ff076b5bb915f5e8492b4a37412
Arash Asady <aasady88@gmail.com>
2017-07-24 11:29:47 -0400

    Checkpoint 1

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,37 @@
-print("HELLO")
+menu="""
+    ------------------------------------
+            PRODUCTS APPLICATION
+    ------------------------------------
+    Hello {0},
+    There are {1} products (Exit and reopen app to refresh product count)
+    Please choose an operation:
+    'List'    | Display a list of product identifiers
+    'Show'    | Show information about a product
+    'Create'  | Add a new product
+    'Update'  | Edit an existing product
+    'Destroy' | Delete an existing product
+"""
+chosen_operation = input(menu)
+chosen_operation = chosen_operation.title()
+
+def list_products():
+    print("LISTING PRODUCTS")
+
+def show_product():
+    print("SHOWING A PRODUCT")
+
+def create_product():
+    print("CREATING A PRODUCT")
+
+def update_product():
+    print("UPDATING A PRODUCT")
+
+def destroy_product():
+    print("DESTROYING A PRODUCT")
+
+if chosen_operation == "List": list_products()
+elif chosen_operation == "Show": show_product()
+elif chosen_operation == "Create": create_product()
+elif chosen_operation == "Update": update_product()
+elif chosen_operation == "Destroy": destroy_product()
+else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
